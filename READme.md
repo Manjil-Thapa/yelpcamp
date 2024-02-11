87e42e4fe49a3762b21bc610c1e037741b147428
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2024-02-11 20:29:45 +1000

    added a readme

---
+++ READme.md
@@ -0,0 +1,50 @@
+# YelpCamp - Campground Review Platform
+
+YelpCamp is a web application that allows users to explore and review camping grounds
+across the globe. Users are able to register, log in, submit campground reviews and
+even add images to make their submissions more appealing.
+
+<a href='https://yelpcamp-zil.onrender.com/'>Live Version</a>
+
+## Features
+
+- User Authentication: Securely register and log in to your account, ensuring
+  personalized experience.
+- Campground Reviews: You can share experiences by submittings reviews of campgrounds
+  that includes comments and ratings.
+- Map Integration: Map is integrated via Mapbox API, that displays the locations of
+  each campground, making it easier for users to know the location of the campground.
+- Image Uploads: Image uploads is powered by the Cloudinary API, ensuring seamless
+  and efficient image storage.
+- Database Connectivity: The application uses Node.js, Express.js and MongoDB,
+  storing user information, campground details and reviews.
+- Built and Deployed: YelpCamp is hosted on <a href='https://render.com/'>Render</a>.
+
+## Technologies Used
+
+- JavaScript
+- CSS
+- Node.js
+- Express.js
+- MongoDB Atlas
+- Mapbox API
+- Cloudinary API
+- Render
+
+## Screenshots
+
+#### Welcome Page
+
+<img src='public/images/sc1.png' width='500' />
+
+#### Login Page
+
+<img src='public/images/sc2.png' width='500' />
+
+#### Submit a new campground site
+
+<img src='public/images/sc3.png' width='500' />
+
+#### Campground site page
+
+<img src='public/images/sc4.png' width='500' />
--- models/campground.js
+++ models/campground.js
@@ -2,62 +2,67 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Review = require('./review');
 
-// setup virtual property which can only be added to a schema (lesson398 for mongoose virtual). add properties to a schema but dont exist in the database but have access to it 
+// setup virtual property which can only be added to a schema (lesson398 for mongoose virtual). add properties to a schema but dont exist in the database but have access to it
 
-const ImageSchema = new Schema ({               // this aint a schema model. wont be exporting
-    url: String,
-    filename: String
-})
-ImageSchema.virtual('thumbnail').get(function() {           // coming from mongoose. not stored in mongo. since url is alrd stored
-    return this.url.replace('/upload', '/upload/w_250');           // replacing url /upload to /upload/w_250
-})
+const ImageSchema = new Schema({
+  // this aint a schema model. wont be exporting
+  url: String,
+  filename: String,
+});
+ImageSchema.virtual('thumbnail').get(function () {
+  // coming from mongoose. not stored in mongo. since url is alrd stored
+  return this.url.replace('/upload', '/upload/w_250'); // replacing url /upload to /upload/w_250
+});
 
-const opts = { toJSON: { virtuals: true }};                         // to stringify json virtuals to show pop up for our e.g on the map
+const opts = { toJSON: { virtuals: true } }; // to stringify json virtuals to show pop up for our e.g on the map
 
-
-const CampgroundSchema = new Schema ({
+const CampgroundSchema = new Schema(
+  {
     title: String,
     images: [ImageSchema],
     geometry: {
-        type: {
-            type: String,
-            enum: ['Point'],
-            required: true
-        },
-        coordinates: {
-            type: [Number],
-            required: true
-        }
+      type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
     price: Number,
     description: String,
     location: String,
     author: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+      type: Schema.Types.ObjectId,
+      ref: 'User',
     },
     reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Review'                       // referencing to review model to get object id
-        }
-    ]
-}, opts);
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Review', // referencing to review model to get object id
+      },
+    ],
+  },
+  opts
+);
 
-CampgroundSchema.virtual('properties.popUpMarkup').get(function() {
-    return `<strong><a href='/campgrounds/${this._id}'>${this.title}</a><strong>
-    <p>${this.description.substring(0, 25)}</p>`
-})
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+  return `<strong><a href='/campgrounds/${this._id}'>${this.title}</a><strong>
+    <p>${this.description.substring(0, 25)}</p>`;
+});
 
-CampgroundSchema.post('findOneAndDelete', async function (doc) {    //query middleware
-    if (doc) {
-        await Review.deleteMany({
-            _id: {
-                $in: doc.reviews
-            }
-        })
-    }
-});                                                       // passing the option to run json virtuals
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+  //query middleware
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+}); // passing the option to run json virtuals
 
 const Campground = mongoose.model('Campground', CampgroundSchema);
 module.exports = Campground;
